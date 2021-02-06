b679b7ebbe478b65a258974ee96ddcdc6dd7c7ea
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-02-06 20:28:13 +0530

    update

---
+++ application/logs/log-2021-01-29.php
@@ -0,0 +1,63 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-01-29 17:26:01 --> Severity: Warning --> mysqli::__construct(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) C:\xampp\htdocs\Test\savsoftquiz_v5\application\config\config.php 515
+ERROR - 2021-01-29 17:35:20 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\Test\savsoftquiz_v5\application\config\config.php 524
+ERROR - 2021-01-29 17:44:10 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-01-29 17:46:58 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-01-29 17:49:00 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-01-29 18:22:55 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-01-29 18:23:14 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-01-29 18:23:21 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-01-29 18:24:24 --> Severity: Warning --> implode(): Invalid arguments passed C:\xampp\htdocs\Test\savsoftquiz_v5\application\models\Quiz_model.php 193
+ERROR - 2021-01-29 18:24:37 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-01-29 18:24:50 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-01-29 18:25:08 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\profile.php 110
+ERROR - 2021-01-29 18:25:08 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\profile.php 199
+ERROR - 2021-01-29 18:26:02 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\profile.php 110
+ERROR - 2021-01-29 18:26:02 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\profile.php 199
+ERROR - 2021-01-29 18:31:17 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\Test\savsoftquiz_v5\application\models\Quiz_model.php 878
+ERROR - 2021-01-29 18:33:11 --> Severity: Warning --> mail(): Failed to connect to mailserver at &quot;localhost&quot; port 587, verify your &quot;SMTP&quot; and &quot;smtp_port&quot; setting in php.ini or use ini_set() C:\xampp\htdocs\Test\savsoftquiz_v5\system\libraries\Email.php 1896
+ERROR - 2021-01-29 18:33:11 --> Could not find the language line "hello"
+ERROR - 2021-01-29 18:33:11 --> Could not find the language line "user_id"
+ERROR - 2021-01-29 18:33:12 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ') order by FIELD(savsoft_options.qid,)' at line 1 - Invalid query: select * from savsoft_options where qid in () order by FIELD(savsoft_options.qid,)
+ERROR - 2021-01-29 18:33:12 --> Severity: error --> Exception: Call to a member function result_array() on boolean C:\xampp\htdocs\Test\savsoftquiz_v5\application\models\Quiz_model.php 316
+ERROR - 2021-01-29 18:51:48 --> Could not find the language line "hello"
+ERROR - 2021-01-29 18:51:48 --> Could not find the language line "user_id"
+ERROR - 2021-01-29 18:51:54 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ') order by FIELD(savsoft_options.qid,)' at line 1 - Invalid query: select * from savsoft_options where qid in () order by FIELD(savsoft_options.qid,)
+ERROR - 2021-01-29 18:51:54 --> Severity: error --> Exception: Call to a member function result_array() on boolean C:\xampp\htdocs\Test\savsoftquiz_v5\application\models\Quiz_model.php 316
+ERROR - 2021-01-29 18:52:49 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\Test\savsoftquiz_v5\application\models\Quiz_model.php 878
+ERROR - 2021-01-29 18:53:22 --> Severity: Warning --> mail(): Failed to connect to mailserver at &quot;localhost&quot; port 587, verify your &quot;SMTP&quot; and &quot;smtp_port&quot; setting in php.ini or use ini_set() C:\xampp\htdocs\Test\savsoftquiz_v5\system\libraries\Email.php 1896
+ERROR - 2021-01-29 18:53:23 --> Could not find the language line "hello"
+ERROR - 2021-01-29 18:53:23 --> Could not find the language line "user_id"
+ERROR - 2021-01-29 18:53:23 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\models\Quiz_model.php 746
+ERROR - 2021-01-29 18:53:23 --> Query error: Unknown column 'NAN' in 'field list' - Invalid query: UPDATE `savsoft_result` SET `total_time` = 0, `end_time` = 1611926603, `score_obtained` = 0, `percentage_obtained` = NAN, `manual_valuation` = 0, `result_status` = 'Pass'
+WHERE `rid` IS NULL
+ERROR - 2021-01-29 18:53:24 --> Severity: Warning --> mail(): Failed to connect to mailserver at &quot;localhost&quot; port 587, verify your &quot;SMTP&quot; and &quot;smtp_port&quot; setting in php.ini or use ini_set() C:\xampp\htdocs\Test\savsoftquiz_v5\system\libraries\Email.php 1896
+ERROR - 2021-01-29 18:53:26 --> Severity: error --> Exception: Too few arguments to function Result::view_result(), 0 passed in C:\xampp\htdocs\Test\savsoftquiz_v5\system\core\CodeIgniter.php on line 532 and exactly 1 expected C:\xampp\htdocs\Test\savsoftquiz_v5\application\controllers\Result.php 133
+ERROR - 2021-01-29 18:53:39 --> Severity: error --> Exception: Too few arguments to function Result::view_result(), 0 passed in C:\xampp\htdocs\Test\savsoftquiz_v5\system\core\CodeIgniter.php on line 532 and exactly 1 expected C:\xampp\htdocs\Test\savsoftquiz_v5\application\controllers\Result.php 133
+ERROR - 2021-01-29 18:53:42 --> Severity: error --> Exception: Too few arguments to function Result::view_result(), 0 passed in C:\xampp\htdocs\Test\savsoftquiz_v5\system\core\CodeIgniter.php on line 532 and exactly 1 expected C:\xampp\htdocs\Test\savsoftquiz_v5\application\controllers\Result.php 133
+ERROR - 2021-01-29 18:53:44 --> Severity: error --> Exception: Too few arguments to function Result::view_result(), 0 passed in C:\xampp\htdocs\Test\savsoftquiz_v5\system\core\CodeIgniter.php on line 532 and exactly 1 expected C:\xampp\htdocs\Test\savsoftquiz_v5\application\controllers\Result.php 133
+ERROR - 2021-01-29 18:54:17 --> Severity: error --> Exception: Too few arguments to function Result::view_result(), 0 passed in C:\xampp\htdocs\Test\savsoftquiz_v5\system\core\CodeIgniter.php on line 532 and exactly 1 expected C:\xampp\htdocs\Test\savsoftquiz_v5\application\controllers\Result.php 133
+ERROR - 2021-01-29 18:54:19 --> Severity: error --> Exception: Too few arguments to function Result::view_result(), 0 passed in C:\xampp\htdocs\Test\savsoftquiz_v5\system\core\CodeIgniter.php on line 532 and exactly 1 expected C:\xampp\htdocs\Test\savsoftquiz_v5\application\controllers\Result.php 133
+ERROR - 2021-01-29 18:54:26 --> Severity: error --> Exception: Too few arguments to function Result::view_result(), 0 passed in C:\xampp\htdocs\Test\savsoftquiz_v5\system\core\CodeIgniter.php on line 532 and exactly 1 expected C:\xampp\htdocs\Test\savsoftquiz_v5\application\controllers\Result.php 133
+ERROR - 2021-01-29 18:54:36 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-01-29 18:55:32 --> Could not find the language line "hello"
+ERROR - 2021-01-29 18:55:32 --> Could not find the language line "user_id"
+ERROR - 2021-01-29 18:56:32 --> Could not find the language line "appointment_timing"
+ERROR - 2021-01-29 18:58:32 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-01-29 19:04:06 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-01-29 19:04:16 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-01-29 19:04:17 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-01-29 19:19:10 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-01-29 19:48:24 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-01-29 20:04:56 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-01-29 20:05:55 --> Could not find the language line "hello"
+ERROR - 2021-01-29 20:05:55 --> Could not find the language line "user_id"
+ERROR - 2021-01-29 20:07:38 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\Test\savsoftquiz_v5\application\models\Quiz_model.php 878
+ERROR - 2021-01-29 20:08:12 --> Severity: Warning --> mail(): Failed to connect to mailserver at &quot;localhost&quot; port 587, verify your &quot;SMTP&quot; and &quot;smtp_port&quot; setting in php.ini or use ini_set() C:\xampp\htdocs\Test\savsoftquiz_v5\system\libraries\Email.php 1896
+ERROR - 2021-01-29 20:08:14 --> Could not find the language line "hello"
+ERROR - 2021-01-29 20:08:14 --> Could not find the language line "user_id"
+ERROR - 2021-01-29 20:08:52 --> Could not find the language line "hello"
+ERROR - 2021-01-29 20:08:52 --> Could not find the language line "user_id"
+ERROR - 2021-01-29 20:08:56 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-01-29 20:40:14 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
