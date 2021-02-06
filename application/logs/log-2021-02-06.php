2505c97c35013ca35920d2d2d429618579b443d8
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-02-06 20:28:26 +0530

    update

---
+++ application/logs/log-2021-02-06.php
@@ -0,0 +1,24 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-02-06 18:02:33 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-02-06 18:05:55 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-02-06 19:09:11 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-02-06 19:09:28 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-02-06 19:09:35 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-02-06 19:14:45 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-02-06 19:15:24 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-02-06 19:24:05 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-02-06 19:24:17 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-02-06 19:24:20 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-02-06 19:30:14 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ')' at line 1 - Invalid query: select * from savsoft_group where gid in () 
+ERROR - 2021-02-06 19:30:14 --> Severity: error --> Exception: Call to a member function result_array() on boolean C:\xampp\htdocs\Test\savsoftquiz_v5\application\controllers\Login.php 157
+ERROR - 2021-02-06 19:32:14 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ')' at line 1 - Invalid query: select * from savsoft_group where gid in () 
+ERROR - 2021-02-06 19:32:14 --> Severity: error --> Exception: Call to a member function result_array() on boolean C:\xampp\htdocs\Test\savsoftquiz_v5\application\controllers\Login.php 157
+ERROR - 2021-02-06 19:32:16 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ')' at line 1 - Invalid query: select * from savsoft_group where gid in () 
+ERROR - 2021-02-06 19:32:16 --> Severity: error --> Exception: Call to a member function result_array() on boolean C:\xampp\htdocs\Test\savsoftquiz_v5\application\controllers\Login.php 157
+ERROR - 2021-02-06 19:34:37 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ')' at line 1 - Invalid query: select * from savsoft_group where gid in () 
+ERROR - 2021-02-06 19:34:37 --> Severity: error --> Exception: Call to a member function result_array() on boolean C:\xampp\htdocs\Test\savsoftquiz_v5\application\controllers\Login.php 157
+ERROR - 2021-02-06 19:42:14 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-02-06 19:42:38 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-02-06 19:42:41 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
+ERROR - 2021-02-06 19:42:41 --> Severity: Warning --> Division by zero C:\xampp\htdocs\Test\savsoftquiz_v5\application\views\dashboard.php 237
