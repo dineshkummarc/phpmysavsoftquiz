f61ff5eb9dd6913755c9ccef602c2710f2f2fa6b
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-03-06 20:09:38 +0530

    update

--- application/config/sq_config.php
+++ application/config/sq_config.php
@@ -1,5 +1,5 @@
 <?php 
-		$sq_base_url='http://local.rocket.co.in:8081/Test/savsoftquiz_v5/';
+		$sq_base_url='http://local.rocket.co.in:8081/test/phpmysavsoftquiz/';
 		$sq_hostname='localhost';
 		$sq_dbname='savsoftquiz_v5_0';
 		$sq_dbusername='root';
